e2219e72a5b89323349ff7db74bc43bfd60f16d8
wangchun <1164004116@qq.com>
2018-08-08 00:13:15 +0800

    增加按扭事件

--- src/main/java/com/duowenlvshi/crawler/lawyercase/service/impl/WebDriverServiceImpl.java
+++ src/main/java/com/duowenlvshi/crawler/lawyercase/service/impl/WebDriverServiceImpl.java
@@ -38,24 +38,38 @@ public class WebDriverServiceImpl implements WebDriverService {
 
     @Override
     public void test() {
-        System.setProperty("webdriver.chrome.driver", "E:\\wangchun\\Intelli_workspace\\crawler\\src\\main\\resources\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\wangchun\\IdeaProjects\\git\\lawyercase\\src\\main\\resources\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         ((ChromeDriver) driver).setErrorHandler(new CommonErrorHandler(false));
         driver.get("http://wenshu.court.gov.cn/");
         try {
             driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-            WebElement searchBtn = ((ChromeDriver) driver).findElementByClassName("head_search_btn");
+
+            WebElement head_maxsearch_btn = driver.findElement(By.xpath("//*[@id=\"head_maxsearch_btn\"]"));
+            head_maxsearch_btn.click();
+            Thread.sleep(1000);
+            WebElement beginTimeCPRQ = driver.findElement(By.xpath("//*[@id=\"beginTimeCPRQ\"]"));
+            beginTimeCPRQ.clear();
+            beginTimeCPRQ.sendKeys("2018-08-06 ");
+            WebElement endTimeCPRQ = driver.findElement(By.xpath("//*[@id=\"endTimeCPRQ\"]"));
+            endTimeCPRQ.clear();
+            endTimeCPRQ.sendKeys(" 2018-08-06");
+
+            WebElement searchBtn = driver.findElement(By.className("head_search_btn"));
             searchBtn.click();
-            ///content/content
             driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
             //获取当前页面句柄
             String current_handle = driver.getWindowHandle();
+            WebElement pageInfo = driver.findElement(By.xpath("//*[@id=\"pageNumber\"]"));
+            String total = pageInfo.getAttribute("total");
+            log.info("查询到的总页数 ->{} ", total);
             List<WebElement> webElements = ((ChromeDriver) driver).findElements(By.ByXPath.xpath("//*[@id=\"resultList\"]/div/table/tbody/tr[1]/td/div/a[2]"));
             for (WebElement element : webElements) {
                 String href = element.getAttribute("href");
                 if (StringUtils.isNotBlank(href) && href.contains("/content/content")) {
-                    driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
-                    element.click();
+                    //driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
+                    log.info(href);
+                    //element.click();
                 }
             }
 
